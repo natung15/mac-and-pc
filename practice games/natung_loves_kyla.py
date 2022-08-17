89a30e4e3046059a9a8c4252813f327f37cadf43
natung15 <107604487+natung15@users.noreply.github.com>
2022-08-16 23:03:12 -0700

    kyla game upgrade

--- practice games/natung_loves_kyla.py
+++ practice games/natung_loves_kyla.py
@@ -1,14 +1,14 @@
 from base64 import encode
 gamestatus = True
 while gamestatus == True:
-    answer = input("Hi there how are you?").lower()
+    answer = input("Hi there how are you? >").lower()
     if answer != ("bad"):
         print("I love you kyla :3")
         print("Come give me a hug")
-        hug_or_not = input("Do you give him a hug? [Yes] or [No]").lower()
+        hug_or_not = input("Do you give him a hug? [Yes] or [No] >").lower()
         if hug_or_not == ("yes"):
             print('Nathan embraces you')
-            print('Nathan says "I Love You')
+            print('Nathan says "I Love You"')
             print(":*")
             break
         if hug_or_not == ("no"):
@@ -16,7 +16,7 @@ while gamestatus == True:
             break
     if answer == ("bad"):
         print("Aweee poor baby come give me a hug and kiss :*")
-        hug_kiss = input(":* or no")
+        hug_kiss = input(":* or no >")
         if hug_kiss == (":*"):
             print(":**** I Love you now come here")
             break
