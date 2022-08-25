842e77fe9f449329dc8ce7beecd7702855869f13
natung15 <107604487+natung15@users.noreply.github.com>
2022-08-25 01:20:31 -0700

    math in cpp

--- cplus/1helloworld.cpp
+++ cplus/1helloworld.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     int file_size = 100;
-    double sales = 9.99
+    double sales = 9.99;
     std::cout << file_size;
     return 0;
 }
---
+++ cplus/3pemdas.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a =3;
+    int b = 10;
+    int c = a + b;
+    std::cout << c;
+    return 0;
+}
