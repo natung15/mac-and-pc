ed1030f4387f73dbe8f237f6c17dd5d01e2a8e84
natung15 <107604487+natung15@users.noreply.github.com>
2022-10-14 11:08:16 -0700

    half arrow finished

--- zybook python practice/4.28DrawingHalfAnArrow.py
+++ zybook python practice/4.28DrawingHalfAnArrow.py
@@ -3,8 +3,16 @@
 
 height = int(input("Height:"))
 width = int(input("Width:"))
+arrowWidth = int(input("Arrow Width:"))
 
 
 for x in range(height):
     for x in range(width):
-        print("*")
+        print("*",end="")
+    print(" ")
+
+for y in range(arrowWidth,0,-1):
+    for x in range(y):
+        print("*",end="")
+    print("")
+
