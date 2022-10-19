8848e917b0b62f5cb4a878cff9087490df2eedb1
natung15 <107604487+natung15@users.noreply.github.com>
2022-10-19 14:02:47 -0700

    practice

--- cplus/1helloworld.cpp
+++ cplus/1helloworld.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main() {
-    int file_size = 100;
-    double sales = 9.99;
-    std::cout << file_size;
+int number1;
+int number2;
+int sum;
+
+cin >> number1;
+cin >> number2;
+sum = number1 * number2;
+
+cout << sum;
+
+
+
+
+
+
+
+
     return 0;
 }
