ccdbf3fbe6c6a33ac954b7fd1c68dfa60b3d3452
natung15 <107604487+natung15@users.noreply.github.com>
2022-11-06 19:45:34 -0800

    game setup

---
+++ comp51/Gametest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+bool gameStatus = true;
+int input;
+
+while(gameStatus == true){
+cin >> input;
+cout << input;
+
+gameStatus = false;
+}
+
+return 0;
+}
